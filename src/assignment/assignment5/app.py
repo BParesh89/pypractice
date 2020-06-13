7b3b6b10ed81302ab42bca4a34b6b20982c5fdf8
Paresh Bhatia <bhatia.paresh89@gmail.com>
2020-06-13 19:28:43 +0530

    commit python flask web app with simple login, register, profile and edi t profile facility

---
+++ src/assignment/assignment5/app.py
@@ -0,0 +1,161 @@
+# Reference --> https://codeshack.io/login-system-python-flask-mysql/
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+app = Flask(__name__)
+
+# Change this to your secret key (can be anything, it's for extra protection)
+app.secret_key = 'er#tr%$'
+
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '5.SOme1@'
+app.config['MYSQL_DB'] = 'pythonlogin'
+
+# Intialize MySQL
+mysql = MySQL(app)
+
+# render home page
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return redirect(url_for('home'))
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('index.html', msg=msg)
+
+
+# http://localhost:5000/python/logout - this will be the logout page
+@app.route('/login/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
+
+# http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
+@app.route('/login/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
+@app.route('/login/home')
+def home():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+# http://localhost:5000/pythinlogin/profile - this will be the profile page, only accessible for loggedin users
+@app.route('/login/profile')
+def profile():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+@app.route('/login/editprofile',methods=['GET','POST'])
+def editprofile():
+    msg = ''
+
+    # Check if user is loggedin
+    if ('loggedin' in session) and request.method=='GET':
+        # Show the profile page with account info
+        return render_template('editprofile.html', username=session['username'])
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method=='POST' and ('password' in request.form or 'email' in request.form):
+        # Create variables for easy access
+        username = session['username']
+        password = request.form['password'].strip()
+        email = request.form['email'].strip()
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor()
+        if password=='' and email!= '':
+            cursor.execute('UPDATE accounts SET email = %s WHERE username = %s', (email,username,))
+        elif email=='' and password!='':
+            cursor.execute('UPDATE accounts SET password = %s WHERE username = %s', (password,username,))
+        elif email!='' and password!='':
+            cursor.execute('UPDATE accounts SET email = %s, password = %s WHERE username = %s', (email, password, username,))
+        if password=='' and email =='':
+            msg='Please enter new password or new email or both to update profile!'
+            return render_template('editprofile.html', username=username, msg=msg)
+        else:
+            mysql.connection.commit()
+            msg='You have successfully updated your profile!'
+            # We need all the account info for the user so we can display it on the profile page
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM accounts WHERE username = %s', (session['username'],))
+            account = cursor.fetchone()
+            return render_template('profile.html', account=account, msg=msg)
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+        return render_template('editprofile.html', username=username, msg=msg)
+
+
+
+# run app on flask server. debug=False should be in production
+if __name__ == '__main__':
+    app.run(debug=True)
